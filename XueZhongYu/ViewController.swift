2299e94f781d35f52da5ef34bf3a54662fc8d872
Tieu Tri Kiet <tieutrikiet@gmail.com>
2019-10-20 21:24:23 +0700

    Initial Commit

---
+++ XueZhongYu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XueZhongYu
+//
+//  Created by Kiệt Tiêu on 10/20/19.
+//  Copyright © 2019 KietTieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
